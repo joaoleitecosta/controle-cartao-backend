ce970f886e61f25d62abe17c1263ea3c7ee3840a
Joao Leite <joao_leite@hotmail.com.br>
2020-03-03 14:42:47 -0300

    ajustando porta no server.js

--- src/server.js
+++ src/server.js
@@ -4,4 +4,4 @@ import options from './config/documentation';
 const expressSwagger = require('express-swagger-generator')(app);
 
 expressSwagger(options);
-app.listen(3001);
+app.listen(processo.env.PORT || 5000);
